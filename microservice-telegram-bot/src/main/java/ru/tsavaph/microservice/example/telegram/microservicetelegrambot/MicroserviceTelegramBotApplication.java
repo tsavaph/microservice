ac8d68a1405e2fc3c8de2516bfb92cd3611e18f0
tsavaph <tsavaph@gmail.com>
2023-05-02 19:08:26 +0300

    inited telegram microservice with all required dependencies

---
+++ microservice-telegram-bot/src/main/java/ru/tsavaph/microservice/example/telegram/microservicetelegrambot/MicroserviceTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package ru.tsavaph.microservice.example.telegram.microservicetelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceTelegramBotApplication.class, args);
+	}
+
+}
---
+++ microservice-telegram-bot/src/test/java/ru/tsavaph/microservice/example/telegram/microservicetelegrambot/MicroserviceTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tsavaph.microservice.example.telegram.microservicetelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
